80a5a8cb744f168b56b9558afec7b5406f400848
vaishnavi16dongre <y47624745@gmail.com>
2025-12-28 18:05:50 +0530

    reacted app

---
+++ vite-app/react-vite/src/Card.jsx
@@ -0,0 +1,21 @@
+
+
+import './Card.css'
+
+function Card(){
+    return(
+        <>
+            <div>
+                <p>
+                    Name= vaishnavi Dongre
+                    Age=21
+
+                </p>
+                
+
+               
+            </div>
+        </>
+    )
+}
+export default Card
--- vite-app/react-vite/src/main.jsx
+++ vite-app/react-vite/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
 import App from './App.jsx'
+import Card from './Card.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <App/>
+  
+    <Card/>
+    
   </StrictMode>,
 )
