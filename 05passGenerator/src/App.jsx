dba3a0cb7800429d37a65a90d9c6a4bdd54f7100
vaishnavi16dongre <y47624745@gmail.com>
2026-01-05 23:31:20 +0530

    Password Generator

---
+++ 05passGenerator/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <h1>Password Generator</h1>
+
+   <div className='box'>
+
+   </div>
+    </>
+  )
+}
+
+export default App
